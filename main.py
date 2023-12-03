063bc45357ea607ba63b41656a0da8af3fe567cd
Piyush_07 <luitelpiyush95@gmail.com>
2023-12-03 21:39:34 +0545

    a black window is created for the first time

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from tkinter import*
 from tkinter import ttk
-from PIL import Image,ImageTk
+# from PIL import Image,ImageTk
+
+class Face_Recognition_System:
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry("1530x790+0+0")
+        self.root.title("Face Recognition System")
 
 
+
+if __name__=="__main__":
+    root=Tk()
+    obj=Face_Recognition_System(root)
+    root.mainloop()
