37a10a1c55fadc86b945646db8da1dfc8bddd3af
Piyush_07 <luitelpiyush95@gmail.com>
2023-12-04 16:41:18 +0545

    some helpful comments are added

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+# Tkinter library to create a graphical user interface (GUI)
 from tkinter import*
+# ttk: Themed Tkinter, a module within Tkinter that provides access to the Tk themed widget set.
 from tkinter import ttk
+# library for opening, manipulating, and saving many different image file formats.
 # from PIL import Image,ImageTk
 
 class Face_Recognition_System:
     def __init__(self,root):
+        # self.root is an instance variable that holds the reference to the main window
         self.root=root
         screen_width = root.winfo_screenwidth()
         screen_height = root.winfo_screenheight()
@@ -16,6 +20,8 @@ class Face_Recognition_System:
 
 
 if __name__=="__main__":
+    # An instance of the Tk class (which represents the main window) is created and assigned to the variable root.
     root=Tk()
     obj=Face_Recognition_System(root)
+    # The mainloop() method starts the Tkinter event loop, and the GUI window is displayed.
     root.mainloop()
